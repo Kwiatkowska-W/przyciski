9df0811031c42596611b981d5f8a82a4a9e996a3
Kwiatkowska-W <wikikwit2004@gmail.com>
2021-12-15 14:47:59 +0100

    ustawiane rotacjiX, skali Y, alphy z aplikacji dla obrazka

--- app/src/main/java/xml/example/przyciski/MainActivity.kt
+++ app/src/main/java/xml/example/przyciski/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.przycisk).setOnClickListener {
         //    findViewById<Button>(R.id.przycisk).text="działa"
             findViewById<Button>(R.id.przycisk).text=findViewById<EditText>(R.id.wprowdztext).text
+            findViewById<ImageView>(R.id.obrazek).rotationX=findViewById<EditText>(R.id.rotacjaX).text.toString().toFloat()
+            findViewById<ImageView>(R.id.obrazek).scaleY=findViewById<EditText>(R.id.SkalaY).text.toString().toFloat()
+            findViewById<ImageView>(R.id.obrazek).alpha=findViewById<EditText>(R.id.alpha).text.toString().toFloat()
         }
     }
 }
